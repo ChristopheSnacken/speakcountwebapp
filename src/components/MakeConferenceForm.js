43366f8619ff517c5562d303a99c96fc399a1119
Arjen <alphabravoab@gmail.com>
2018-07-24 11:24:31 +0200

    should have done this yesterday

---
+++ src/components/MakeConferenceForm.js
@@ -0,0 +1,77 @@
+import React, { PureComponent } from 'react'
+import { connect } from 'react-redux'
+
+
+  class MakeConferenceForm extends PureComponent {
+    state = {}
+
+	handleSubmit = (e) => {
+		e.preventDefault()
+		this.props.onSubmit(this.state)
+		this.setState({title:'', message:'' })
+	}
+
+	handleChange = (event) => {
+    const {name, value} = event.target
+
+    this.setState({
+      [name]: value
+    })
+  }
+
+
+    render() {
+      return(
+			<form onSubmit={this.handleSubmit}>
+				<div>
+					<label htmlFor="title">title</label>
+					<input type="title" name="title" id="title" value={
+						this.state.title || ''
+					} onChange={ this.handleChange } />
+				</div>
+
+                <div>
+					<label htmlFor="date">date</label>
+					<input type="date" name="date" id="date" value={
+						this.state.date || ''
+					} onChange={ this.handleChange } />
+				</div>
+                
+                <div>
+					<label htmlFor="time">time</label>
+					<input type="time" name="time" id="time" value={
+						this.state.time || ''
+					} onChange={ this.handleChange } />
+				</div>
+
+                 <div>
+					<label htmlFor="EstimatedDuration">EstimatedDuration</label>
+					<input type="time" name="EstimatedDuration" id="EstimatedDuration" value={
+						this.state.EstimatedDuration || ''
+					} onChange={ this.handleChange } />
+				</div>
+            <div>
+                <select onChange = { this.handleChange }>
+                    <option value = ""> participants </option>
+                    <option value = "2"> 2 </option>
+                    <option value = "3"> 3 </option>
+                    <option value = "4"> 4 </option>
+                    <option value = "5"> 5 </option>
+                    <option value = "6"> 6 </option>
+                    <option value = "7"> 7 </option>
+                    <option value = "8"> 8 </option>
+                    <option value = "9"> 9 </option>
+                    <option value = "10"> 10 </option>
+                </select>
+            </div>
+				<button type="submit">Proceed</button>
+			</form>
+         )
+       }
+  }
+
+
+
+
+
+  export default connect( ) (MakeConferenceForm)
---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux'
+//import otherReducer from './otherReducer'
+
+export default combineReducers({
+  //otherReducer
+})
